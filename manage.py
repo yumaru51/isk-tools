0261c2f125e63e2a9aae4e3d80e35801858e1b60
y-kawauchi <yumaru51@yahoo.co.jp>
2023-04-03 19:45:04 +0900

    settings.pyを変更。不要なapp消す。 manage.pyを変更

--- manage.py
+++ manage.py
@@ -2,11 +2,22 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from socket import gethostname
 
-
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+if __name__ == '__main__':
+    host_name = gethostname()
+    if host_name == 'YWEBSERV1':
+        print('本番環境起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_product')
+    elif host_name == 'I7161DD6':
+        print('テスト環境起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_test')
+    elif host_name == 'I7781DN7':
+        print('開発環境1起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_develop1')
+    else:
+        print('開発環境2起動')
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings_develop2')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
