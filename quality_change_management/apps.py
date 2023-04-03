0ee03bdc5508974f5687f702414720a8b17e2c0c
y-kawauchi <yumaru51@yahoo.co.jp>
2023-04-03 19:42:52 +0900

    プロジェクト作成 ライブラリインストール。requirements.txtを丸投げ。 .gitignore丸投げ。 templateは丸投げ。 django-admin startproject config . python manage.py startapp fms python manage.py startapp quality_change_management

---
+++ quality_change_management/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QualityChangeManagementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quality_change_management'
